544e8d139d3a6a438e72de1fe0b9d1ee09ec75e9
Divyansh Singh <dsingh07@wm.edu>
2026-03-06 01:47:09 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://divysingh07.github.io',
   base: '/Portfolio',
-  integrations: [tailwind()],
 });
